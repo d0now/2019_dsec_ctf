0ad703258e1d95cadc586d702af32f6c5b47a10e
d0now <dolpin1402@gmail.com>
2019-10-28 01:29:41 -0700

    qemu -s (debug flag) add

--- images/debug/run.sh
+++ images/debug/run.sh
@@ -9,4 +9,5 @@ qemu-system-x86_64              \
     -append 'console=ttyS0 loglevel=3 oops=panic panic=-1 quiet kaslr' \
     -cpu kvm64,+smap,+smep      \
     -monitor /dev/null          \
+    -s                          \
     2>/dev/null
